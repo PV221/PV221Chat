3cda59156d3d6bc1840e75f7e0f542eea11b62d5
GoodJoker1 <antonstrakhov55@gmail.com>
2024-09-26 19:46:05 +0200

    Add methods to db repository and interface

--- ClassLibrary1/Interfaces/IUserRepository.cs
+++ ClassLibrary1/Interfaces/IUserRepository.cs
@@ -4,4 +4,6 @@ namespace PV221Chat.Core.Interfaces;
 
 public interface IUserRepository : IDataRepository<User>
 {
+    Task<bool> IsPresent(string email);
+    Task<bool> IsValidUser(string email, string password);
 }
