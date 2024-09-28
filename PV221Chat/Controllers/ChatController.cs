f8551bb98a6d51f3d53bc7fcb5016d5165fcedcd
GoodJoker1 <antonstrakhov55@gmail.com>
2024-09-28 20:01:29 +0200

    Update/add code

---
+++ PV221Chat/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PV221Chat.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PV221Chat/ViewComponents/ChatsListViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PV221Chat.DTO;
+
+namespace PV221Chat.ViewComponents
+{
+    public class ChatsListViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var chats = await GetChatsAsync(); 
+            return View(chats);
+        }
+
+        private Task<List<ChatDTO>> GetChatsAsync()
+        {
+            // Zastąp to prawdziwym pobraniem danych
+            return Task.FromResult(new List<ChatDTO>
+        {
+            new ChatDTO { ChatId = 1, ChatName = "Chat 1", IsOpen = true },
+            new ChatDTO { ChatId = 2, ChatName = "Chat 2", IsOpen = false }
+        });
+        }
+    }
+}
