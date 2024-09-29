ab7c3936d7aceedd3deaf3734ff2b04c2141307c
GoodJoker1 <antonstrakhov55@gmail.com>
2024-09-29 17:34:43 +0200

    Add/Update componets chat list

--- PV221Chat/ViewComponents/ChatListViewComponent.cs
+++ PV221Chat/ViewComponents/ChatListViewComponent.cs
@@ -1,24 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using PV221Chat.Core.DataModels;
+using PV221Chat.Core.Interfaces;
 using PV221Chat.DTO;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
 
 namespace PV221Chat.ViewComponents
 {
     public class ChatListViewComponent : ViewComponent
     {
+        private readonly IChatRepository _chatRepository;
+        private readonly IUserRepository _userRepository;
+        public ChatListViewComponent(IChatRepository chatRepository, IUserRepository userRepository)
+        {
+            _chatRepository = chatRepository;
+            _userRepository = userRepository;
+        }
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var chats = await GetChatsAsync(); 
             return View(chats);
         }
 
-        private Task<List<ChatDTO>> GetChatsAsync()
+        private async Task<List<ChatDTO>> GetChatsAsync()
         {
-            // Zastąp to prawdziwym pobraniem danych
-            return Task.FromResult(new List<ChatDTO>
-        {
-            new ChatDTO { ChatId = 1, ChatName = "Chat 1", IsOpen = true },
-            new ChatDTO { ChatId = 2, ChatName = "Chat 2", IsOpen = false }
-        });
+            var claimsPrincipal = User as ClaimsPrincipal;
+            var email = claimsPrincipal?.FindFirst(ClaimTypes.Email)?.Value;
+
+            if (string.IsNullOrEmpty(email))
+            {
+                return new List<ChatDTO>();
+            }
+
+            var user = await _userRepository.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return new List<ChatDTO>();
+            }
+
+            var chats = await _chatRepository.GetDataByUserIdAsync(user.UserId);
+
+            // Zamień IEnumerable<Chat> na List<ChatDTO>
+            return temp(chats).ToList();
         }
+
+        private IEnumerable<ChatDTO> temp(IEnumerable<Chat> chats)
+        {
+            return chats.Select(chat => 
+                new ChatDTO {
+                    ChatId = chat.ChatId,
+                    ChatName= chat.ChatName,
+                    ChatType= chat.ChatType,
+                    IsOpen= chat.IsOpen,
+                    CreatedAt= chat.CreatedAt
+                });
+        }
+
     }
 }
