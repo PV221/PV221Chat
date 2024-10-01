6754abc3db65b45cc7ae0c2fe42a0bd2acd5c2f5
GoodJoker1 <antonstrakhov55@gmail.com>
2024-10-01 18:45:02 +0200

    Update Repository

--- ClassLibrary1/Interfaces/IChatRepository.cs
+++ ClassLibrary1/Interfaces/IChatRepository.cs
@@ -5,4 +5,5 @@ namespace PV221Chat.Core.Interfaces;
 public interface IChatRepository : IDataRepository<Chat>
 {
     Task<IEnumerable<Chat>> GetDataByUserIdAsync(int userId);
+    Task<IEnumerable<User>> GetUsersByChatId(int chatId);
 }
