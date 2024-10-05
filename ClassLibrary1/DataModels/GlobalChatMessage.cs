156e5f93835f081d366dd54b5d967ee5f38f7931
GoodJoker1 <antonstrakhov55@gmail.com>
2024-10-05 21:19:38 +0200

    Edit/Add data models

---
+++ ClassLibrary1/DataModels/GlobalChatMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PV221Chat.Core.DataModels;
+
+public partial class GlobalChatMessage
+{
+    public int UserId { get; set; }
+
+    public int MessageGcId { get; set; }
+
+    public string MessageText { get; set; } = null!;
+
+    public DateTime CreateAt { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
