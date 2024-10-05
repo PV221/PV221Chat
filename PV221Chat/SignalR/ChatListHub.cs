91c63db0a7da028698230fd871c49da3070ad89b
GoodJoker1 <antonstrakhov55@gmail.com>
2024-10-05 12:39:28 +0200

    Commented because it doesn't work(error)

--- PV221Chat/SignalR/ChatListHub.cs
+++ PV221Chat/SignalR/ChatListHub.cs
@@ -4,23 +4,23 @@ namespace PV221Chat.SignalR;
 
 public class ChatListHub : Hub
 {
-    public override async Task OnConnectedAsync()
-    {
-        var chatId = Context.GetHttpContext().Request.Query["chatId"];
-        await Groups.AddToGroupAsync(Context.ConnectionId, chatId);
-        await base.OnConnectedAsync();
-    }
+    //public override async Task OnConnectedAsync()
+    //{
+    //    var chatId = Context.GetHttpContext().Request.Query["chatId"];
+    //    await Groups.AddToGroupAsync(Context.ConnectionId, chatId);
+    //    await base.OnConnectedAsync();
+    //}
 
-    public override async Task OnDisconnectedAsync(Exception exception)
-    {
-        var chatId = Context.GetHttpContext().Request.Query["chatId"];
-        await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatId);
-        await base.OnDisconnectedAsync(exception);
-    }
+    //public override async Task OnDisconnectedAsync(Exception exception)
+    //{
+    //    var chatId = Context.GetHttpContext().Request.Query["chatId"];
+    //    await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatId);
+    //    await base.OnDisconnectedAsync(exception);
+    //}
 
-    public async Task SendNotificationToGroup(int chatId, string message, int unreadCount)
-    {
-        await Clients.Group(chatId.ToString())
-            .SendAsync("ReceiveNotification", chatId, message, unreadCount);
-    }
+    //public async Task SendNotificationToGroup(int chatId, string message, int unreadCount)
+    //{
+    //    await Clients.Group(chatId.ToString())
+    //        .SendAsync("ReceiveNotification", chatId, message, unreadCount);
+    //}
 }
