358c8467e6effe16529a075065308ba337a10941
GoodJoker1 <antonstrakhov55@gmail.com>
2024-10-05 12:33:35 +0200

    Add method to controller(send message)

--- PV221Chat/Controllers/ChatController.cs
+++ PV221Chat/Controllers/ChatController.cs
@@ -4,18 +4,25 @@ using PV221Chat.Core.DataModels;
 using PV221Chat.Core.Interfaces;
 using PV221Chat.Core.Repositories;
 using PV221Chat.Models;
+using PV221Chat.Services;
+using PV221Chat.Services.Interfaces;
 using System;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace PV221Chat.Controllers
 {
     [Authorize]
     public class ChatController : Controller
     {
+        private readonly IUserRepository _userRepository;
         private readonly IChatRepository _chatRepository;
-        public ChatController(IChatRepository chatRepository)
+        private readonly IMessageExtension _messageExtension;
+        public ChatController(IChatRepository chatRepository, IMessageExtension messageExtension, IUserRepository userRepository)
         {
             _chatRepository = chatRepository;
+            _messageExtension = messageExtension;
+            _userRepository = userRepository;
         }
         public IActionResult Index()
         {
@@ -40,6 +47,18 @@ namespace PV221Chat.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(int chatId, string message)
+        {
+            var claimsPrincipal = User as ClaimsPrincipal;
+            var email = claimsPrincipal?.FindFirst(ClaimTypes.Email)?.Value;
+            var user = await _userRepository.FindByEmailAsync(email);
+
+            var messageDTO = await _messageExtension.SendMessageAsync(chatId, message, user.UserId);
+
+            return Ok(messageDTO);
         }
     }
 }
