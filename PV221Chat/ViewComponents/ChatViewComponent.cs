f795b5e0d440caab46e3929b26b8f4a76f9ea80e
GoodJoker1 <antonstrakhov55@gmail.com>
2024-10-05 12:37:00 +0200

    Update InvokeAsync method(get normal data) in ChatViewComponent

--- PV221Chat/ViewComponents/ChatViewComponent.cs
+++ PV221Chat/ViewComponents/ChatViewComponent.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PV221Chat.Core.DataModels;
 using PV221Chat.Core.Interfaces;
 using PV221Chat.Core.Repositories;
 using PV221Chat.DTO;
+using System.Security.Claims;
 
 namespace PV221Chat.ViewComponents
 {
@@ -20,14 +22,40 @@ namespace PV221Chat.ViewComponents
 
         public async Task<IViewComponentResult> InvokeAsync(int chatId)
         {
-            var messages = new List<MessageDTO>() { 
-                new MessageDTO{},
-                new MessageDTO{}
-            };
+            ViewBag.ChatId = chatId;
 
-            return View(messages);
+            var messages = await _messageRepository.GetByChatIdAndSortedAsync(chatId, 100);
+
+            return View(await ConvertMessageToMessageDTO(messages));
         }
+
+        private async Task<List<MessageDTO>> ConvertMessageToMessageDTO(IEnumerable<Message> messages)
+        {
+            var result = new List<MessageDTO>();
+
+            var claimsPrincipal = User as ClaimsPrincipal;
+            var email = claimsPrincipal?.FindFirst(ClaimTypes.Email)?.Value;
+            var user = await _userRepository.FindByEmailAsync(email);
+
+            foreach (var message in messages) 
+            {
+
+                result.Add(new MessageDTO
+                {
+                    MessageId = message.MessageId,
+                    ChatId = message.ChatId,
+                    SenderId = message.SenderId,
+                    MessageType = message.SenderId.ToString() == user.UserId.ToString() ? "sent" : "received",
+                    SenderName = (await _userRepository.GetDataAsync((int)message.SenderId)).Nickname,
+                    MessageText = message.MessageText,
+                    SentAt = message.SentAt,
+                    IsDeleted = message.IsDeleted
+                });
+                
+            }
+
+            return result;
+        }
+
     }
-
-
 }
