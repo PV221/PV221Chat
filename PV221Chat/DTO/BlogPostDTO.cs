91929070c50f0b376f0fc4494f91ba4ddad0b8b1
Atfan <atfan235@gmail.com>
2024-10-06 09:37:34 +0300

    create

---
+++ PV221Chat/DTO/BlogPostDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace PV221Chat.DTO
+{
+    public class BlogPostDTO
+    {
+        public int BlogPostId { get; set; }
+
+        public int BlogPageId { get; set; }
+
+        public string? Title { get; set; }
+
+        public string Text { get; set; } = null!;
+
+        public DateTime? CreateAt { get; set; }
+    }
+}
---
+++ PV221Chat/Mapper/BlogPostMapper.cs
@@ -0,0 +1,38 @@
+﻿using PV221Chat.Core.DataModels;
+using PV221Chat.DTO;
+
+namespace PV221Chat.Mapper
+{
+    public static class BlogPostMapper
+    {
+        public static BlogPost ToModel(BlogPostDTO dto)
+        {
+            return new BlogPost
+            {
+                Title = dto.Title,
+                Text = dto.Text,
+                CreateAt = dto.CreateAt,
+                BlogPageId = dto.BlogPageId
+            };
+        }
+        public static BlogPostDTO ToDTO(BlogPost model)
+        {
+            return new BlogPostDTO
+            {
+                Title = model.Title,
+                Text = model.Text,
+                CreateAt = model.CreateAt,
+                BlogPageId = model.BlogPageId,
+                BlogPostId = model.BlogPostId
+            };
+        }
+        public static void UpdateModel(BlogPostDTO dto, BlogPost model)
+        {
+            if (!string.IsNullOrEmpty(dto.Title))
+                model.Title = dto.Title;
+
+            if (!string.IsNullOrEmpty(dto.Text))
+                model.Text = dto.Text;
+        }
+    }
+}
