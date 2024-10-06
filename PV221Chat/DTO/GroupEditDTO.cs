945e9dec9ef6943f464a50c37464ef6d407cfd6c
GoodJoker1 <antonstrakhov55@gmail.com>
2024-10-06 10:25:09 +0200

    Add dto

---
+++ PV221Chat/DTO/GroupEditDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PV221Chat.DTO
+{
+    public class GroupEditDTO
+    {
+        public int chatId { get; set; }
+        public string GroupName { get; set; }
+        public List<UserDTO> users { get; set; }
+    }
+}
