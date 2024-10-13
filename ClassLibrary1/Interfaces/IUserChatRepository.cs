c8faa9d1242663ace9d2a6ecdd67ca83b7a30803
GoodJoker1 <antonstrakhov55@gmail.com>
2024-10-13 12:58:56 +0200

    delete repositories and service in core

--- ClassLibrary1/Interfaces/IUserChatRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using PV221Chat.Core.DataModels;
-
-namespace PV221Chat.Core.Interfaces;
-
-public interface IUserChatRepository : IDataRepository<UserChat>
-{
-    Task<UserChat?> FindUserChatByUserIdAndChatIdAsync(int userId, int chatId);
-    Task<IEnumerable<UserChat>> GetAllUserChatsAsync(int chatId);
-}
--- ClassLibrary1/Interfaces/IUserRatingRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿using PV221Chat.Core.DataModels;
-
-namespace PV221Chat.Core.Interfaces;
-
-public interface IUserRatingRepository : IDataRepository<UserRating>
-{
-}
